e70a3184013d4bf99ac91d813afd52bbd4709d12
luizg <luizg@LUIZ-GASPARINI>
2022-09-05 17:55:18 -0300

    Atualização do código.

--- Estudo/src/PrimeiroPrograma.java
+++ Estudo/src/PrimeiroPrograma.java
@@ -4,7 +4,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Olá Mundo");
-	
+	System.out.println("Teste");
 	
 	
 	
